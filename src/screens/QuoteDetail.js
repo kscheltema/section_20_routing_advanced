120cf8faaaad6bb09919659e83449ec2382f9716
kscheltema <k87scheltema@outlook.com>
2021-06-25 11:57:45 +0200

    nested comments with dynamic Route

--- src/screens/QuoteDetail.js
+++ src/screens/QuoteDetail.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuoteAll = () => {
   const params = useParams();
@@ -8,7 +9,9 @@ const QuoteAll = () => {
     <Fragment>
       <h1>All Quotes</h1>
       <p>{params.quoteID}</p>
-      <Route path={`/quotes/${params.quoteID}/comments`}></Route>
+      <Route path={`/quotes/${params.quoteID}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
