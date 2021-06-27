a1fda6cb44196b81d6f276611d537517d5230fff
kscheltema <k87scheltema@outlook.com>
2021-06-27 17:21:58 +0200

    conditional rendering based on route path

--- src/screens/QuoteDetail.js
+++ src/screens/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -20,6 +20,16 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteID}`} exact>
+        <div className="centered">
+          <Link
+            className={"btn--flat"}
+            to={`/quotes/${params.quoteID}/comments`}
+          >
+            load comments
+          </Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${params.quoteID}/comments`}>
         <Comments />
       </Route>
