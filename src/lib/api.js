8ead99055a42e2c188071c05eae1b0acf90c877b
kscheltema <k87scheltema@outlook.com>
2021-06-27 18:25:15 +0200

    my firebase

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,5 @@
-const FIREBASE_DOMAIN = "https://react-prep-default-rtdb.firebaseio.com";
+const FIREBASE_DOMAIN =
+  "https://burgerbuilder-89b34-default-rtdb.firebaseio.com/";
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
