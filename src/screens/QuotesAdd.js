5d3854cffa84138158cb8412c8d6960ae3f6591c
kscheltema <k87scheltema@outlook.com>
2021-06-27 15:00:36 +0200

    progamatically navigate based on user action

--- src/screens/QuotesAdd.js
+++ src/screens/QuotesAdd.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const QuoteAdd = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
