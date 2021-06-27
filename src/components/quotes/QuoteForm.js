424a87f883947b9c11de09b75530fd97933899fc
kscheltema <k87scheltema@outlook.com>
2021-06-27 15:25:25 +0200

    conditional removal of Promt omnipresent Promt

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -26,6 +26,10 @@ const QuoteForm = (props) => {
     setIsEntering(true);
   };
 
+  const finishedEnteringHandler = () => {
+    setIsEntering(false);
+  };
+
   return (
     <Fragment>
       <Prompt
@@ -55,7 +59,9 @@ const QuoteForm = (props) => {
             <textarea id="text" rows="5" ref={textInputRef}></textarea>
           </div>
           <div className={classes.actions}>
-            <button className="btn">Add Quote</button>
+            <button onClick={finishedEnteringHandler} className="btn">
+              Add Quote
+            </button>
           </div>
         </form>
       </Card>
