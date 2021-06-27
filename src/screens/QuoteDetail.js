e9d9baf490a3a0e78a099cec9ed1c536c26e3e7f
kscheltema <k87scheltema@outlook.com>
2021-06-27 17:51:23 +0200

    routing dynamically defined as extracted from App

--- src/screens/QuoteDetail.js
+++ src/screens/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useRouteMatch, useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -9,6 +9,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteID);
@@ -20,7 +21,7 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteID}`} exact>
+      <Route path={`${match.path}`} exact>
         <div className="centered">
           <Link
             className={"btn--flat"}
@@ -30,7 +31,7 @@ const QuoteDetail = () => {
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteID}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
