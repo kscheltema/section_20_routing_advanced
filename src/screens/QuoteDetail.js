20b6f4e759e03f7f87bb26e969ed76172403539f
kscheltema <k87scheltema@outlook.com>
2021-06-27 14:07:38 +0200

    dynamic display of qyoutes

--- src/screens/QuoteDetail.js
+++ src/screens/QuoteDetail.js
@@ -1,14 +1,25 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
-const QuoteAll = () => {
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
+
+const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteID);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
 
   return (
     <Fragment>
-      <h1>All Quotes</h1>
-      <p>{params.quoteID}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteID}/comments`}>
         <Comments />
       </Route>
@@ -16,4 +27,4 @@ const QuoteAll = () => {
   );
 };
 
-export default QuoteAll;
+export default QuoteDetail;
